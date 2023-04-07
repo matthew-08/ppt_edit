2b88dee7975fdc01505078de62835d23372788fa
Matthew <matthewjcrosby1@gmail.com>
2023-04-07 14:47:40 +0700

    Roguh outline in place for iterating through slides

--- ppt.py
+++ ppt.py
@@ -1,6 +1,9 @@
 import collections 
 import collections.abc
 from pptx import Presentation
+from pptx.dml.color import RGBColor
+from pptx.enum.dml import MSO_THEME_COLOR
+from pptx.util import Pt
 
 
 prs = Presentation('./ppt-templates/explodingkittens.pptx')
@@ -30,15 +33,18 @@ prs.save('test.pptx') """
 slides = prs.slides
 
 counter = 0
+allSlides = []
 for slide in prs.slides:
+    questionAndAnswer = []
     for shape in slide.shapes:
-        if shape.name == "Header Textbox":
-            print('test')
-            counter +=1
+        if shape.name == "Header Textbox" or shape.name == "Answer Textbox":
             for nested_shape in shape.shapes:
                 if nested_shape.has_text_frame:
-                    print(nested_shape.text)
-
-
-
-
+                    default = nested_shape.text_frame.paragraphs[0].runs[0]       
+                    questionAndAnswer.append(default)
+        if shape.name == "Answer Textbox":
+            allSlides.append(questionAndAnswer)
+for q, a in allSlides:
+    print(q.text)
+    print(a.text)
+prs.save('game-test.pptx')
