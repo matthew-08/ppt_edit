de0261af5a43f3f4cffc9e692b6607a891c2f522
Matthew <matthewjcrosby1@gmail.com>
2023-04-07 13:07:48 +0700

    This code is nasty, tbh

--- ppt.py
+++ ppt.py
@@ -3,20 +3,24 @@ import collections.abc
 from pptx import Presentation
 
 
-prs = Presentation('example.pptx')
-
+prs = Presentation('./ppt-templates/explodingkittens.pptx')
+most_recent_not_shape = 0
 
 text_runs = []
-
 for slide in prs.slides:
     for shape in slide.shapes:
         if not shape.has_text_frame:
+            if most_recent_not_shape != 0:
+                if most_recent_not_shape.name == "Header Textbox":
+                    print(dir(most_recent_not_shape))
+
+            most_recent_not_shape = shape
             continue
         for paragraph in shape.text_frame.paragraphs:
-            temp = dict({slide.slide_id: []})
             for run in paragraph.runs:
-                temp[slide.slide_id].append(run.text)
-                text_runs.append(temp)
-for k in text_runs:
-    print(k.items())
+                text_runs.append(run.text)
+                if(run.text == 'Additional text or pictures go here'):
+                    print('test')
+                print(run.text)
 prs.save('test.pptx')
+
