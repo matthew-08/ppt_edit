d482c378759ee671878ab467d0ea18f9eb1044e6
Matthew <matthewjcrosby1@gmail.com>
2023-04-07 13:21:25 +0700

    Created function for finding header text content

--- ppt.py
+++ ppt.py
@@ -7,12 +7,15 @@ prs = Presentation('./ppt-templates/explodingkittens.pptx')
 most_recent_not_shape = 0
 
 text_runs = []
-for slide in prs.slides:
+""" for slide in prs.slides:
     for shape in slide.shapes:
         if not shape.has_text_frame:
             if most_recent_not_shape != 0:
                 if most_recent_not_shape.name == "Header Textbox":
-                    print(dir(most_recent_not_shape))
+                    for nested_shape in most_recent_not_shape.shapes:
+                        if nested_shape.has_text_frame:
+                           print(dir(nested_shape))
+
 
             most_recent_not_shape = shape
             continue
@@ -22,5 +25,20 @@ for slide in prs.slides:
                 if(run.text == 'Additional text or pictures go here'):
                     print('test')
                 print(run.text)
-prs.save('test.pptx')
+prs.save('test.pptx') """
+
+slides = prs.slides
+
+counter = 0
+for slide in prs.slides:
+    for shape in slide.shapes:
+        if shape.name == "Header Textbox":
+            print('test')
+            counter +=1
+            for nested_shape in shape.shapes:
+                if nested_shape.has_text_frame:
+                    print(nested_shape.text)
+
+
+
 
